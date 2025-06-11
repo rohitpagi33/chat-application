91357aa3b9283016bf27a79e17c9126695ec0e28
Prince61141 <princepatel61141@gmail.com>
2025-06-11 19:55:51 +0530

    Update CORS

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,8 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:5173',
+    origin: ['https://chat-application-iv4q.vercel.app/',
+      'http://localhost:5173'],
     methods: ['GET', 'POST'],
     credentials: true,
   },
